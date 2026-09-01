105eebbe7d4ccb54aca5c705dffdd3a7973083e6
Vanushan <jhcvanushan001@gmail.com>
2026-09-01 21:10:00 +0530

    feat: add weapon assets, create prefabs, and place them in the scene

--- Zombie_Survival_Last_Stand/Assets/Scripts/Bullet.cs
+++ Zombie_Survival_Last_Stand/Assets/Scripts/Bullet.cs
@@ -16,12 +16,23 @@ public class Bullet : MonoBehaviour
         {
             print("hit " + objectWeHit.gameObject.name + " !");
 
+            CreateBulletImpactEffect(objectWeHit);
+
             // Apply impact force to the target Rigidbody to knock it down
             Rigidbody targetRb = objectWeHit.gameObject.GetComponent<Rigidbody>();
             if (targetRb != null)
             {
-                targetRb.AddForce(transform.forward * 10f, ForceMode.Impulse);
+                targetRb.AddForce(transform.forward * 15f, ForceMode.Impulse);
+                targetRb.useGravity = true;
             }
+
+            Destroy(gameObject);
+        }
+        if (objectWeHit.gameObject.CompareTag("Wall"))
+        {
+            print("hit " + objectWeHit.gameObject.name + " !");
+
+            CreateBulletImpactEffect(objectWeHit);
 
             Destroy(gameObject);
         }
@@ -30,5 +41,14 @@ public class Bullet : MonoBehaviour
             // Destroy on any other solid collision
             Destroy(gameObject);
         }
+    }
+
+    void CreateBulletImpactEffect(Collision objectWeHit)
+    {
+         ContactPoint contact = objectWeHit.contacts[0];
+
+         GameObject hole = Instantiate(GlobalReferences.Instance.bulletImpactEffectPrefab, contact.point, Quaternion.LookRotation(contact.normal));
+
+         hole.transform.SetParent(objectWeHit.gameObject.transform);
     }
 }
